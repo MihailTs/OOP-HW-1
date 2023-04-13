5c7862d5c8c9651df81b298014263b5acb40375b
MihailTs <mihail.tsvetkov.04@gmail.com>
2023-04-13 11:29:39 +0300

    additions to Car

--- Car.cpp
+++ Car.cpp
@@ -1,5 +1,52 @@
 #include "Car.h"
+#include <cstring>
+
+void Car::setID(int newID){
+    ID = newID;
+}
+
+void Car::setNumber(char* newNumber){
+    strcpy(number, newNumber);
+}
+
+void Car::setRentSum(double sum){
+    rentSum = sum;
+}
+
+void Car::setPosition(Position p){
+    position.x = p.x;
+    position.y = p.y;
+}
+
+void Car::setRented(bool r){
+    rented = r;
+}
 
 int Car::getID() const{
     return ID;
+}
+
+int Car::getRentSum() const{
+    return rentSum;
+}
+
+char* Car::getNumber() const{
+    return number;
+}
+
+Position Car::getPosition() const{
+    return position;
+}
+
+bool Car::isRented() const{
+    return rented;
+}
+
+Car& Car::operator=(const Car& c){
+    setID(c.getID());
+    setNumber(c.getNumber());
+    setRentSum(c.getRentSum());
+    setPosition(c.getPosition());
+    setRented(c.isRented());
+    return *this;
 }
--- Car.h
+++ Car.h
@@ -6,14 +6,34 @@ class Car{
     private:
         int ID;
         //стандартен за България номер от 8 символа
-        char number[9];
+        char* number = new char[9];
         double rentSum;
-        Position pos;
+        Position position;
+        bool rented;
+
+        void setID(int);
+
+        void setNumber(char*);
+
+        void setRentSum(double);
+
+        void setPosition(Position p);
+
+        void setRented(bool);
 
     public:
 
         int getID() const;
 
+        char* getNumber() const;
+
+        int getRentSum() const;
+
+        Position getPosition() const;
+
+        bool isRented() const;
+
         //operator=
+        Car& operator=(const Car&);
 
 };
