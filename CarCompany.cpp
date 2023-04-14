1807b8892d977caa887dfd9b7d7378a419fd6468
MihailTs <mihail.tsvetkov.04@gmail.com>
2023-04-14 10:58:22 +0300

    Company modified

--- CarCompany.cpp
+++ CarCompany.cpp
@@ -1,7 +1,67 @@
 #include "CarCompany.h"
 
-Car* CarCompany::getCars(){
+void CarCompany::setClientCount(unsigned cnt){
+    clientCount = cnt;
+}
+
+void CarCompany::setCarCount(unsigned cnt){
+    carCount = cnt;
+}
+
+void CarCompany::setCarsCapacity(unsigned cap){
+    carsCapacity = cap;
+}
+
+void CarCompany::setClientsCapacity(unsigned cap){
+    clientsCapacity = cap;
+}
+
+CarCompany::CarCompany(const CarCompany& carComp){
+    setCarCount(carComp.getCarCount());
+    setCarsCapacity(carComp.getCarsCapacity());
+    cars = new Car[getCarsCapacity()];
+    for(int i = 0; i < getCarCount(); i++)
+        cars[i] = carComp.getCars()[i];
+
+    setClientCount(carComp.getClientCount());
+    setClientsCapacity(carComp.getClientsCapacity());
+    clients = new Client[getClientsCapacity()];
+    for(int i = 0; i < getClientCount(); i++)
+        clients[i] = carComp.getClients()[i];
+}
+
+CarCompany::CarCompany(){
+    cars = new Car[1];
+    carCount = 0;
+    carsCapacity = 1;
+
+    clients = new Client[1];
+    clientCount = 0;
+    clientsCapacity = 1;
+}
+
+Car* CarCompany::getCars() const{
     return cars;
+}
+
+Client* CarCompany::getClients() const{
+    return clients;
+}
+
+unsigned CarCompany::getCarCount() const{
+    return carCount;
+}
+
+unsigned CarCompany::getClientCount() const{
+    return clientCount;
+}
+
+unsigned CarCompany::getCarsCapacity() const{
+    return carsCapacity;
+}
+
+unsigned CarCompany::getClientsCapacity() const{
+    return clientsCapacity;
 }
 
 void CarCompany::addCar(const Car& c){
@@ -25,12 +85,30 @@ void CarCompany::addCar(const Car& c){
     carCount += 1;
 }
 
+void CarCompany::addClient(const Client& c){
+    for(int i = 0; i < clientCount; i++)
+        if(clients[i].getID() == c.getID() ) return;
 
-//сигурни сме, че всяка кола в списъка е с уникално ID
+    //проверка за надхвърляне капацитета на масива и разширяване
+    if(clientCount >= clientsCapacity){
+        clientsCapacity += 10;
+        Client* newClients = new Client[clientsCapacity];
+
+        for(int i = 0; i < clientCount; i++)
+            newClients[i] = clients[i];
+
+        delete []clients;
+        clients = newClients;
+    }
+    //добавяне на коолата
+    clients[clientCount] = c;
+    clientCount += 1;
+
+}
+
 void CarCompany::removeCarByID(int ID){
     for(int i = 0; i < carCount; i++)
         if(cars[i].getID() == ID){
-            //разменяме последната и тази
             cars[i] = cars[carCount-1];
             carCount--;
     }
@@ -39,8 +117,12 @@ void CarCompany::removeCarByID(int ID){
 void CarCompany::removeClient(int IDNumber){
     for(int i = 0; i < carCount; i++)
         if(clients[i].getID() == IDNumber){
-            //разменяме последнатя и този
             clients[i] = clients[clientCount-1];
             clientCount--;
     }
+}
+
+CarCompany::~CarCompany(){
+    delete []clients;
+    delete []cars;
 }
--- CarCompany.h
+++ CarCompany.h
@@ -12,13 +12,42 @@ class CarCompany{
         unsigned clientCount;
         unsigned clientsCapacity;
 
+        void setClientCount(unsigned);
+
+        void setCarCount(unsigned);
+
+        void setClientsCapacity(unsigned);
+
+        void setCarsCapacity(unsigned);
+
     public:
 
-        Car* getCars();
+        CarCompany();
+
+        CarCompany(const CarCompany&);
+
+        Car* getCars() const;
+
+        Client* getClients() const;
+
+        unsigned getCarCount() const;
+
+        unsigned getClientCount() const;
+
+        unsigned getCarsCapacity() const;
+
+        unsigned getClientsCapacity() const;
 
         void addCar(const Car&);
+
+        void addClient(const Client&);
 
         void removeCarByID(int);
 
         void removeClient(int);
+
+        //няма оператор =, защото се предполага,
+        //че ще има само една компания        
+
+        ~CarCompany();
 };
